783b3a2e67c7bdcc2ac8d7591628dac1eb370f87
Connor Fulton <2347048F@student.gla.ac.uk>
2018-02-05 14:11:25 +0000

    Near end of Ch9, added user login functionality

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page, PageAdmin
+from rango.models import Category, Page, PageAdmin, UserProfile
 
 
 admin.site.register(Page, PageAdmin)
@@ -8,3 +8,4 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
