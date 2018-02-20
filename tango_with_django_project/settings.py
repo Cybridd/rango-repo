153ed39f7bb15ebc5ed4acda1269ec66f459625c
Connor Fulton <2347048F@student.gla.ac.uk>
2018-02-20 16:49:19 +0000

    End of Ch11, switched to django-registration-redux and added password change functionality

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -134,4 +135,10 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-LOGIN_URL = '/rango/login/'
+# Registration
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
